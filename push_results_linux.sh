b98e8b95748c7ae5c58c6856be0b5fb881b46131
faircm2 <faircm2@github.com>
2026-06-02 23:11:24 +0200

    push_results_linux.sh: updated paths for flat repo structure

--- push_results_linux.sh
+++ push_results_linux.sh
@@ -3,13 +3,13 @@ SCRIPT_NAME=$1
 SRC_IMAGES=$2
 REPO=/opt/lbm/Lb-Python
 
-mkdir -p ${REPO}/results/freesurface/${SCRIPT_NAME}
+mkdir -p ${REPO}/FreesurfaceImages/${SCRIPT_NAME}
 
 # Only copy final snapshots
-find "${SRC_IMAGES}" -name "phi_snapshot*"        | sort | tail -1 | xargs -I{} cp {} ${REPO}/results/freesurface/${SCRIPT_NAME}/
-find "${SRC_IMAGES}" -name "density_map*"         | sort | tail -1 | xargs -I{} cp {} ${REPO}/results/freesurface/${SCRIPT_NAME}/
-find "${SRC_IMAGES}" -name "*channel_parameters*" | xargs -I{} cp {} ${REPO}/results/freesurface/${SCRIPT_NAME}/
-find "${SRC_IMAGES}" -name "*Metrics*"            | xargs -I{} cp {} ${REPO}/results/freesurface/${SCRIPT_NAME}/
+find "${SRC_IMAGES}" -name "phi_snapshot*"        | sort | tail -1 | xargs -I{} cp {} ${REPO}/FreesurfaceImages/${SCRIPT_NAME}/
+find "${SRC_IMAGES}" -name "density_map*"         | sort | tail -1 | xargs -I{} cp {} ${REPO}/FreesurfaceImages/${SCRIPT_NAME}/
+find "${SRC_IMAGES}" -name "*channel_parameters*" | xargs -I{} cp {} ${REPO}/FreesurfaceImages/${SCRIPT_NAME}/
+find "${SRC_IMAGES}" -name "*Metrics*"            | xargs -I{} cp {} ${REPO}/FreesurfaceImages/${SCRIPT_NAME}/
 
 cd ${REPO}
 git add -A
